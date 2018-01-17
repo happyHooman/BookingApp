07f81be29584beb3d4af63ddac3ec6422685a353
happyHooman <tornea.alexandru@gmail.com>
2018-01-17 16:33:40 +0200

    component name can not start with 'data-'

--- app/Public/Main.module.js
+++ app/Public/Main.module.js
@@ -2,13 +2,13 @@ import angular from 'angular';
 
 import {createBookingComponent} from './Booking/CreateBooking.component'
 import {defaultModalComponent} from './modals/default-modal/default-modal.component'
-import {dataModalComponent} from './modals/data-modal/data-modal.component'
+import {formModalComponent} from './modals/form-modal/form-modal.component'
 import {calendarModalComponent} from './modals/calendar-modal/calendar-modal.component'
 import {successModalComponent} from './modals/success-modal/success-modal.component'
 
  export default angular.module('Public', [])
      .component('createBookingComponent',createBookingComponent)
      .component('defaultModalComponent',defaultModalComponent)
-     .component('dataModalComponent',dataModalComponent)
+     .component('formModalComponent',formModalComponent)
      .component('calendarModalComponent',calendarModalComponent)
      .component('successModalComponent',successModalComponent)
---
+++ app/Public/modals/form-modal/form-modal.component.js
@@ -0,0 +1,14 @@
+import template from './form-modal.template.html'
+
+class FormModalController {
+	constructor() {
+	}
+	$onInit() {
+  }
+}
+
+export const formModalComponent = {
+	controller: FormModalController,
+	controllerAs: '$ctrl',
+	template
+}
