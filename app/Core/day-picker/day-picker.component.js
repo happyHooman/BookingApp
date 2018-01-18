b6e6087340a4e2b1e5675c90b132f47f4275a5c2
happyHooman <tornea.alexandru@gmail.com>
2018-01-18 14:07:37 +0200

    created component to pick the day and the time of the service

---
+++ app/Core/day-picker/day-picker.component.js
@@ -0,0 +1,16 @@
+import template from './day-picker.template.html'
+
+class DayPickerController {
+  constructor(){
+
+  }
+  $onInit(){
+
+  }
+}
+
+export const dayPickerComponent={
+  controller: DayPickerController,
+  controllerAs: '$ctrl',
+  template
+}
