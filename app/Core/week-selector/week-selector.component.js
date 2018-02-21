4ea83748817f68e4e7730b5982f482ab62b2f827
happyHooman <tornea.alexandru@gmail.com>
2018-02-21 12:01:41 +0200

    finished styling week-selector component

--- app/Core/week-selector/week-selector.component.js
+++ app/Core/week-selector/week-selector.component.js
@@ -2,7 +2,20 @@ import template from './week-selector.template.html'
 
 class WeekSelector {
   constructor() {
+    this.weeks = [
+      `Feb 19 - 25, 2018`,
+      `Feb 26 - Mar 4, 2018`,
+      `Mar 5 - 11, 2018`
+    ]
 
+    }
+  $onInit(){
+    this.week = this.weeks[1]
+  }
+
+  changeWeek(increment){
+    let index = this.weeks.indexOf(this.week)
+    this.week = this.weeks[index + increment]
   }
 }
 
