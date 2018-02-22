f0ee2f6fcad9810614a73f4a9864b1d5c510a490
happyHooman <tornea.alexandru@gmail.com>
2018-02-22 17:19:23 +0200

    finished the cool date-range filter

--- app/Core/date-range-filter/date-range.filter.js
+++ app/Core/date-range-filter/date-range.filter.js
@@ -1,12 +1,19 @@
 function dateRange($filter){
   return input => {
     let month = $filter("date")(input, 'MMM')
-    let monday = input.getDate()
-    console.log('monday:', monday)
-    let sunday = input
-    console.log(input.getDate() + 6)
-    console.log(month)
-    let output = $filter("date")(input, 'd') + ' ' + month
+    let year = input.getFullYear()
+    let sunday = new Date(year, input.getMonth(), input.getDate() + 6)
+    let sundayMonth = $filter("date")(sunday, 'MMM')
+    let output = ''
+    if(month === sundayMonth){
+      output = input.getDate() + ' - ' + sunday.getDate() + ' ' + month + ' ' + year
+    } else {
+      if (year === sunday.getFullYear()) {
+        output = input.getDate() + ' ' + month + ' - ' + sunday.getDate() + ' ' + sundayMonth + ' ' + year
+      } else {
+        output = input.getDate() + ' ' + month + ' ' + year + ' - ' + sunday.getDate() + ' ' + sundayMonth + ' ' + sunday.getFullYear()
+      }
+    }
     return output
   }
 }
