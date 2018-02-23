32bdaf6494519b0ae4ff80fe676a49d670bb3b15
happyHooman <tornea.alexandru@gmail.com>
2018-02-23 11:24:49 +0200

    best go-top button

--- app/Core/apendixes/go-top/go-top.component.js
+++ app/Core/apendixes/go-top/go-top.component.js
@@ -5,6 +5,27 @@ class GoTopController {
   constructor() {
 
   }
+
+  $onInit(){
+    window.onscroll = this.whenScrolling
+  }
+
+  whenScrolling(){
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        document.getElementsByClassName('go-top')[0].style.display = 'block'
+    } else {
+        document.getElementsByClassName('go-top')[0].style.display = 'none'
+    }
+  }
+
+  goTop(){
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+  }
+
+  $onDestroy(){
+    window.onscroll = function (){}
+  }
 }
 
 export const goTopComponent = {
